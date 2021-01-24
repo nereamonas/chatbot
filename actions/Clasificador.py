d3f32836daa16d56cb8333ec5d2b338bf32cb833
monas <48202889+nereamonas@users.noreply.github.com>
2021-01-24 14:59:08 +0100

    Add files via upload

--- actions/Clasificador.py
+++ actions/Clasificador.py
@@ -1,86 +1,143 @@
-import math, re
-from collections import defaultdict
+import os
+import re
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
 from stop_words import get_stop_words
-import nltk.stem
-from langdetect import detect
-
-class Clasificador():
-	def __init__(self):
-		self.clases = defaultdict(lambda : defaultdict(int))
-
-	def dividirPorPalabras(self,text):  # Obtenemos todo el texto y lo dividimos por palabras
-		#Detectar idioma
-		idioma=detect(text)  # el de euskera no detecta. entonces sera o es o otros q equivale a eu
-
-		# 1- Poner to do en minusculas
-		text = text.lower()
-
-		# 2- quitado las url
-		text = re.sub("https?://[^\s]+", "", text)  # quitar las url.
-
-		# 3- quitar get_stop_words('spanish')
-		if ("es"==idioma):
-			stop_words = get_stop_words('spanish')
-		else:
-			stop_words=["al","anitz","arabera","asko","baina","bat","batean","batek","bati","batzuei","batzuek","batzuetan","batzuk","bera","beraiek","berau","berauek","bere","berori","beroriek","beste","bezala","da","dago","dira","ditu","du","dute","edo","egin","ere","eta","eurak","ez","gainera","gu","gutxi","guzti","haiei","haiek","haietan","hainbeste","hala","han","handik","hango","hara","hari","hark","hartan","hau","hauei","hauek","hauetan","hemen","hemendik","hemengo","hi","hona","honek","honela","honetan","honi","hor","hori","horiei","horiek","horietan","horko","horra","horrek","horrela","horretan","horri","hortik","hura","izan","ni","noiz","nola","non","nondik","nongo","nor","nora","ze","zein","zen","zenbait","zenbat","zer","zergatik","ziren","zituen","zu","zuek","zuen","zuten"] #https://github.com/stopwords-iso/stopwords-eu
-
-		for word in stop_words:
-			text=text.replace(" "+word+" "," ") # a, al, algo, algunas...
-
-		# 4- quitar simbolos
-		symbols = "'!\"#$%&()*+-./:;<=>?@[\]^_`{|}~"  #\n /// no puedo quitarle los " jaajaj text = text.replace('"', "")
-		for i in symbols:
-			text=text.replace(i,"")
-
-		# 5- quitar numeros
-		text = re.sub("\d+", "", text)
-
-		# 6- mas de dos letras y letras entre a-z
-		# Dividimos en palabras. ya no es texto
-		palabras = re.findall("[a-z]{2,}", text, re.I)  # [re.I]	It ignores case
-
-		# 7- SnowballStemmer('spanish')
-		if ("es"==idioma):
-			spanish_stemmer = nltk.stem.SnowballStemmer('spanish')
-		else:
-			spanish_stemmer = nltk.stem.SnowballStemmer('spanish')  # creo q no hay esta biblioteca en euskera ns o si jj
-		palabras = [spanish_stemmer.stem(palabra) for palabra in palabras]
-
-		return palabras
-
-	def train(self, texto, clase): # Metodo para entrenar al clasificador
-		palabras = self.dividirPorPalabras(texto)  # conseguimos las palabras
-		# nos guardara en la posicion de cada palabra la cantidad de veces que se repite.  ejemplo: {'videoconferencias': 1, 'con': 35, 'collaborate': 7, 'ultra': 5, 'manual': 6, 'para': 52, 'el': 128}
-		for word in palabras:
-			self.clases[clase][word] += 1
+print(os.getcwd())
+pathAbs=os.getcwd()
 
 
-	def sacarProbabilidad(self,palabras,clase): # Parte logica, la cual etiquetara al texto
-		#AQUI HAY MAS INFO y implementacion mas compleja: https://www.instintoprogramador.com.mx/2019/07/python-para-nlp-crear-un-modelo-tf-idf.html
-		#https://github.com/LuisAlejandroSalcedo/StringTagger-Clasificador-de-Texto
-		palabrasTotales = sum(self.clases[clase].values())  #cuantas palabras hay en cada clase
-		#print (numWords)
-		logSum = 0
-		for palabra in palabras:  # analizaremos cada palabra de la frase
-			#print (self.clases[clase][palabra])
-			freq = self.clases[clase][palabra] + 1  #suma una repeticion
-			#print (freq)
-			prob = freq/palabrasTotales #freq actual de la palabra / palabras totales  TF = (Frequency of the word in the sentence) / (Total number of words in the sentence)
-			#print (prob)
-			logSum += math.log(prob)   #calcula el log. y vamos sumando todos los resultados. IDF: log((Total number of sentences (documents))/(Number of sentences (documents) containing the word))
-			#print(logSum)
+def capar(text):
+    # 1- Poner to do en minusculas
+    text = text.lower()
+    # 2- quitado las url
+    text = re.sub("https?://[^\s]+", "", text)  # quitar las url.
+    # 3- quitar get_stop_words('spanish')
+    stop_words = get_stop_words('spanish')
+    for word in stop_words:
+        text = text.replace(" " + word + " ", " ")  # a, al, algo, algunas...
+    # 4- quitar simbolos
+    symbols = "'!\"#$%&()*+-./:;<=,>?@[\]^_`{|}~“”"  # \n /// no puedo quitarle los " jaajaj text = text.replace('"', "")
+    for i in symbols:
+        text = text.replace(i, "")
+    text=text.replace('"', '')
+    # 5- quitar numeros
+    text = re.sub("\d+", "", text)
+    return text
 
-		return logSum
-	
-	def clasificaPregunta(self,texto): # Rrecibe la pregunt ay la clasifica
-		probs = {}
-		palabras = self.dividirPorPalabras(texto)  #dividimos la pregunta por palabras
-		for clase in self.clases:  # tendrá que mirar por todas las clases cual es su probabilidad para así quedarse con la más alta
-			probs[clase] = self.sacarProbabilidad(palabras,clase)
-		
-		elegida = sorted(probs,key=probs.get)[-1]  #ns quedamos con el de mayor probabilidad
+#1- ENTRENAMIENTO CLASES
+#2- Cargamos el dataset
+df=pd.read_csv(pathAbs+'/archivos/csv/concatenate.csv',sep=',')  # no se porq solo va con ruta absoluta
+#3- imprimimos las longitudes
+print("len total: ",len(df),"\nLen videoconferencia: ",len(df[df.Class=='videoconferencia']),"\nLen cuestionarios: ",len(df[df.Class=='cuestionarios']))
+#3- TfIdf Vectorized. Y separamos para train y test
+cvClase = TfidfVectorizer(min_df=1,stop_words='english')
+x_trainClase=df["Text"]
+y_trainClase=df["Class"]
+count=0
+for x in x_trainClase:
+    x_trainClase[count]=capar(x)
+    count+=1
+#4- fit transform
+x_traincvClase=cvClase.fit_transform(x_trainClase)
+# 6- Clasificador
+mnbClase = MultinomialNB(fit_prior=False)  # 0.930693
+mnbClase.fit(x_traincvClase, y_trainClase)  # lo entreno con el train
 
-		return elegida
+#2- ENTRENAMIENTO videoconferencia
+df = pd.read_csv(pathAbs+'/archivos/csv/VideoconferenciaXTemas_ES.csv', sep=',')  # no se porq solo va con ruta absoluta
+# 3- TfIdf Vectorized. Y separamos para train y test
+cvVideoconferencia = TfidfVectorizer(min_df=1, stop_words='english')
+x_trainVideoconferencia = df["Text"]
+y_trainVideoconferencia = df["Class"]
+count = 0
+for x in x_trainVideoconferencia:
+    x_trainVideoconferencia[count] = capar(x)
+    count += 1
+# 4- fit transform
+x_traincvVideoconferencia = cvVideoconferencia.fit_transform(x_trainVideoconferencia)
+# 6- Clasificador
+mnbVideoconferencia = GridSearchCV(SVC(), [{'kernel': ['rbf'], 'gamma': [1e-3], 'C': [1000]}])
+mnbVideoconferencia.fit(x_traincvVideoconferencia, y_trainVideoconferencia)  # lo entreno con el train
+
+#3- ENTRENAMIENTO CUESTIONARIOS
+df = pd.read_csv(pathAbs+'/archivos/csv/CuestionariosXTemas_ES.csv', sep=',')  # no se porq solo va con ruta absoluta
+# 3- TfIdf Vectorized. Y separamos para train y test
+cvCuestionarios = TfidfVectorizer(min_df=1, stop_words='english')
+x_trainCuestionarios = df["Text"]
+y_trainCuestionarios = df["Class"]
+count = 0
+for x in x_trainCuestionarios:
+    x_trainCuestionarios[count] = capar(x)
+    count += 1
+# 4- fit transform
+x_traincvCuestionarios = cvCuestionarios.fit_transform(x_trainCuestionarios)
+# 6- Clasificador
+mnbCuestionarios = GridSearchCV(SVC(), [{'kernel': ['rbf'], 'gamma': [1e-3], 'C': [1000]}])
+mnbCuestionarios.fit(x_traincvCuestionarios, y_trainCuestionarios)  # lo entreno con el train
+
+def clasificador(text):
+    x_test= [capar(text)]
+    x_testcv = cvClase.transform(x_test)
+    # Sacamos la predicción
+    predicion = mnbClase.predict(x_testcv)
+    # nos dará el threshold de cada clase
+    threshold = mnbClase.predict_proba(x_testcv)
+    return predicion, threshold
 
 
+def buscarManual(clase,text):
+    if (clase == 'videoconferencia'):
+        mnb=mnbVideoconferencia
+        cv=cvVideoconferencia
+    elif (clase == 'cuestionarios'):
+        mnb=mnbCuestionarios
+        cv=cvCuestionarios
 
+    x_test = [capar(text)]
+    x_testcv = cv.transform(x_test)
+    # Sacamos la predicción
+    predicion = mnb.predict(x_testcv)
+    # nos dará el threshold de cada clase
+    #threshold = mnb.predict_proba(x_testcv)  #el de grid no tiene el predict proba q da el threshold. habria q buscar otro modo
+    threshold=0
+    return predicion, threshold
+
+def conseguirPaginaCorrespondiente(clase, tema):
+    # Cargamos el csv que nos dice que pagina del manual corresponde con el tema
+    df = pd.read_csv(pathAbs+'/archivos/csv/paginaCorrespondiente.csv', sep=',')
+    clasee = df.loc[:, 'Class'] == clase
+    df_1 = df.loc[clasee]
+    subclase = df_1.loc[:, 'SubClass'] == int(tema)
+    df_2 = df_1.loc[subclase]
+    pagina = df_2.values[0][2]
+    print("PAGINA  ", pagina)
+    #Una vez tenemos la pagina, tenemos que crear la url para la pagina correspondiente. añadiendo a la url #page=8 - se abre en la apg que queramos
+    url='null'
+    if(clase=='videoconferencia'):
+        url = "https://www.ehu.eus/documents/1852718/14189177/Bilera+birtualak++Collaborate-rekin+Irakasleentzako+eskuliburua.pdf/a393e0d9-29ee-5e8c-9122-d1983a4939d5" + "#page=" + str(pagina)
+    elif(clase=='cuestionarios'):
+        url = "https://www.ehu.eus/documents/1852718/14449606/Cuestionarios-eGela.pdf/541e8924-4d67-bcc6-a208-eff3bd438b6e?t=1588762441000" + "#page=" + str(pagina)
+    return url
+
+def clasificarPregunta(pregunta):
+    clasePredecida, threshold = clasificador(pregunta)
+
+    print(clasePredecida[0])
+    print(threshold[0])
+    if max(threshold[0]) > 0.50:  # si el threshold es >50% clasificaremos como que será correcta la clase que predice
+        clase=clasePredecida[0]
+        # mirar el foro de videoconferencia
+        # si no se en cuentra mirar en el manual de videoconferencia
+        temaPredecido, thresholdTema = buscarManual(clase, pregunta)
+
+        tema = str(temaPredecido[0])
+
+        # Habria que mirar el threshold. pero de momento que lo haga siempre
+        # Conseguimos la url que le corresponde al tema y clase
+        url = conseguirPaginaCorrespondiente(clase, tema)
+
+    return clase,tema,url
--- actions/actions.py
+++ actions/actions.py
@@ -8,11 +8,9 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-#
-from rasa.shared.core.events import SessionStarted, ActionExecuted
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import EventType
 from rasa_sdk.executor import CollectingDispatcher
+from actions.Clasificador import clasificarPregunta
 #
 #
 # class ActionHelloWorld(Action):
@@ -28,22 +26,6 @@ from rasa_sdk.executor import CollectingDispatcher
 #
 #         return []
 
-from Clasificador import Clasificador
-clasificador = Clasificador()  #clasificador
-hola="PEPE"
-training_data = {
-    "videoconferenciaES":['txt/6-Videoconferencia_ES.txt'],
-    "videoconferenciaEU": ['txt/6-Videoconferencia_EUS.txt'],
-    "cuestionarioES": ['txt/8-Cuestionarios_ES.txt'],
-    "cuestionarioEU": ['txt/8-Cuestionarios_EUS.txt'],
-}
-#ENTRENAR
-for categoria, ficheros in training_data.items(): #recorremos todos los elementos del training data
-    for fichero in ficheros:
-        print("Clasificando "+ fichero)
-        f = open(fichero, "r")  #cogemos el fichero
-        contents = f.read()  #guardamos to do su contenido en un atr
-        clasificador.train(contents,categoria)  #lo entrenamos
 
 
 class ActionDefaultFallback(Action):
@@ -58,13 +40,17 @@ class ActionDefaultFallback(Action):
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         pregunta = tracker.latest_message.get('text')
-        resultado = clasificador.clasificaPregunta(pregunta)
-        respuesta="Clase clasificada: "+ resultado
+        clase,tema,url=clasificarPregunta(pregunta)
+        textRespuesta='Clase clasificada: '+clase+ "\nTema predecido: "+tema+"\nUrl: "
+        print(textRespuesta)
+        dispatcher.utter_message(text=textRespuesta)
+        dispatcher.utter_message(attachment=url)
 
-        dispatcher.utter_message(text=respuesta)
+        buttons = [{"payload": "/affirm", "title": "Si"},{"payload": "/deny", "title": "No"}]
+        dispatcher.utter_button_message("¿Te ha servido de ayuda?",buttons)
 
-        #Ahora con la clase mirar en el foro
+        #Me he dado cuenta q los manuales en pdf son mas cortros que los del doc. entonces las paginas no cuadran.
 
-        # si no se en cuentra mirar en el manual
 
-        return []
+# dispatcher.utter_message('<a href="">Click para abrir el manual</a>')
+# dispatcher.utter_message('[Click para abrir el manual](https://www.ehu.eus/documents/1852718/14189177/Bilera+birtualak++Collaborate-rekin+Irakasleentzako+eskuliburua.pdf/a393e0d9-29ee-5e8c-9122-d1983a4939d5)')
